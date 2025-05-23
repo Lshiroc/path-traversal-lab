cde0c5c7d9b3abf0738b9699aae2ec5b7b7ac854
Lshiroc <zeynalmerdanli4321@gmail.com>
2025-05-23 20:38:56 +0400

    added str_end_with polyfill

--- hard/images/index.php
+++ hard/images/index.php
@@ -1,4 +1,14 @@
 <?php
+    if (!function_exists('str_ends_with')) {
+        function str_ends_with($haystack, $needle) {
+            $length = strlen($needle);
+            if ($length == 0) {
+                return true;
+            }
+            return (substr($haystack, -$length) === $needle);
+        }
+    }
+
     $filename = str_replace("....//", "", $_GET["filename"]);
     $filename = str_replace("../", "", $filename);
     if (str_ends_with($filename, ".png") || str_ends_with($filename, ".jpg") 
